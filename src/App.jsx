f6df745954026db159cc429637ba50e7dc3fef00
Subhodip Sadhu <subhodipsadhu9@gmail.com>
2023-10-26 17:29:23 +0530

    added pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 // import React from 'react'
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
